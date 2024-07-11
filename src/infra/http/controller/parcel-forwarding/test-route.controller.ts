60a6adf9b06e42afe30522a474a8bbc1929340be
Pedro Requiao <alonsofts@gmail.com>
2024-07-11 18:35:38 -0400

    Remove large file from tracking 6

--- src/infra/http/controller/parcel-forwarding/test-route.controller.ts
+++ src/infra/http/controller/parcel-forwarding/test-route.controller.ts
@@ -1,32 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common'
-
-import { ZodValidationPipe } from '@/infra/http/pipe/zod-validation-pipe'
-import { z } from 'zod'
-
+import { Controller, Get } from '@nestjs/common'
 import { Public } from '@/infra/auth/public'
-
-const testBodySchema = z.object({
-  message: z.string().optional(),
-})
-
-const bodyValidationPipe = new ZodValidationPipe(testBodySchema)
-
-type TestBodySchema = z.infer<typeof testBodySchema>
 
 @Controller('/test')
 @Public()
 export class TestController {
-  @Post()
-  async handle(@Body(bodyValidationPipe) body: TestBodySchema) {
-    const { message } = body
-
-    if (message) {
-      return {
-        message,
-      }
-    }
-    return {
-      message: 'Hello World',
-    }
+  @Get()
+  async handle() {
+    return 'Hello World'
   }
 }
