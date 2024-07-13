a8e29ee10d5b09ce44ca1a298d52b9126578349d
Pedro Requiao <alonsofts@gmail.com>
2024-07-13 13:41:05 -0400

    Remove large file from tracking 42

--- src/infra/lambda.ts
+++ src/infra/lambda.ts
@@ -1,27 +1,27 @@
-// import { ValidationPipe } from '@nestjs/common'
-// import { NestFactory } from '@nestjs/core'
-// import serverlessExpress from '@vendia/serverless-express'
-// import { Callback, Context, Handler } from 'aws-lambda'
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import serverlessExpress from '@vendia/serverless-express'
+import { Callback, Context, Handler } from 'aws-lambda'
 
-// import { AppModule } from './app.module'
+import { AppModule } from './app.module'
 
-// let server: Handler
+let server: Handler
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule)
-//   app.useGlobalPipes(new ValidationPipe())
-//   await app.init()
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
+  await app.init()
 
-//   const expressApp = app.getHttpAdapter().getInstance()
-//   return serverlessExpress({ app: expressApp })
-// }
+  const expressApp = app.getHttpAdapter().getInstance()
+  return serverlessExpress({ app: expressApp })
+}
 
-// export const handler: Handler = async (
-//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   event: any,
-//   context: Context,
-//   callback: Callback,
-// ) => {
-//   server = server ?? (await bootstrap())
-//   return server(event, context, callback)
-// }
+export const handler: Handler = async (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap())
+  return server(event, context, callback)
+}
