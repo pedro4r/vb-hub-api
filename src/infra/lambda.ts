885531241826e5a504cff0191574d25fafb5eeb2
Pedro Requiao <alonsofts@gmail.com>
2024-07-21 17:31:40 -0400

    test: public routes 10

--- src/infra/lambda.ts
+++ src/infra/lambda.ts
@@ -1,29 +1,21 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { EnvService } from './env/env.service'
 import serverlessExpress from '@vendia/serverless-express'
-import { Handler, Context, Callback } from 'aws-lambda'
+import { Callback, Context, Handler } from 'aws-lambda'
+
+import { AppModule } from './app.module'
 
 let server: Handler
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
 
-  // Configuração de CORS adaptada para o Lambda
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://192.168.1.237:5173',
-      'http://192.168.208.3:5173',
-    ],
-    credentials: false,
+    allowedHeaders: 'Content-Type',
+    origin: '*',
+    methods: 'OPTIONS,POST,GET',
   })
-
-  const configService = app.get(EnvService)
-  // No contexto do Lambda, a porta não é diretamente relevante,
-  // mas você pode precisar dela para outras configurações
-  const port = configService.get('PORT')
-  console.log(`Application is configured to run on port: ${port}`)
 
   await app.init()
 
--- src/infra/main.ts
+++ src/infra/main.ts
@@ -8,14 +8,14 @@ async function bootstrap() {
   })
 
   // Enable CORS
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://192.168.1.237:5173',
-      'http://192.168.208.3:5173',
-    ],
-    credentials: true,
-  })
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:5173',
+  //     'http://192.168.1.237:5173',
+  //     'http://192.168.208.3:5173',
+  //   ],
+  //   credentials: true,
+  // })
 
   const configService = app.get(EnvService)
   const port = configService.get('PORT')
