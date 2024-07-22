df439a9a31b8fcc5469639c883076d75702a364b
Pedro Requiao <alonsofts@gmail.com>
2024-07-22 01:07:36 -0400

    test: public routes 12

--- src/infra/lambda.ts
+++ src/infra/lambda.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
 
-  app.enableCors({})
+  app.enableCors()
 
   await app.init()
 
