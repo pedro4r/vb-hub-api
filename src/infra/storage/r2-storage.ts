32fa3ecfca53d883337c1482a14637a413b82248
Pedro Requiao <alonsofts@gmail.com>
2024-07-23 01:38:37 -0400

    test: lambda function with upload file route 3

--- src/infra/storage/r2-storage.ts
+++ src/infra/storage/r2-storage.ts
@@ -13,6 +13,7 @@ export class R2Storage implements Uploader {
 
   constructor(private envService: EnvService) {
     const accountId = envService.get('CLOUDFLARE_ACCOUNT_ID')
+    console.log('Initializing S3Client with accountId:', accountId)
 
     this.client = new S3Client({
       endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
@@ -22,23 +23,38 @@ export class R2Storage implements Uploader {
         secretAccessKey: envService.get('CLOUDFLARE_SECRET_ACCESS_KEY'),
       },
     })
+
+    console.log('S3Client initialized successfully.')
   }
 
   async upload({ fileType, body }: UploadParams): Promise<{ url: string }> {
     const uploadId = randomUUID()
-    const uniqueFileName = `${uploadId}.${fileType.split('/')[1]}`
+    const extension = fileType.split('/')[1]
+    const uniqueFileName = `${uploadId}.${extension}`
 
-    await this.client.send(
-      new PutObjectCommand({
-        Bucket: this.envService.get('CLOUDFLARE_BUCKET_NAME'),
-        Key: uniqueFileName,
-        ContentType: fileType,
-        Body: body,
-      }),
+    console.log(
+      `Uploading file with name: ${uniqueFileName} and type: ${fileType}`,
     )
 
-    return {
-      url: uniqueFileName,
+    try {
+      const start = Date.now()
+      await this.client.send(
+        new PutObjectCommand({
+          Bucket: this.envService.get('CLOUDFLARE_BUCKET_NAME'),
+          Key: uniqueFileName,
+          ContentType: fileType,
+          Body: body,
+        }),
+      )
+      const end = Date.now()
+      console.log(`Upload completed in ${end - start}ms`)
+
+      return {
+        url: uniqueFileName,
+      }
+    } catch (error) {
+      console.error('Error uploading file:', error)
+      throw error
     }
   }
 }
